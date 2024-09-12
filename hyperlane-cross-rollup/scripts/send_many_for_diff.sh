3759d4e4c18029c0f86d24f88fc7419bb73728cb
bianyuanop <chen.me.nan@gmail.com>
2024-09-12 18:07:37 -0400

    sending txs to multiple domain

---
+++ hyperlane-cross-rollup/scripts/send_many_for_diff.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+GETH_ORIGIN=http://127.0.0.1:9095
+GETH_REMOTE=http://127.0.0.1:9096
+ORIGIN_CHAINID=45205
+REMOTE_CHAINID=45206
+KEY=47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a
+
+go run main.go --geth-origin $GETH_ORIGIN --geth-remote $GETH_REMOTE --origin-chainid $ORIGIN_CHAINID --remote-chainid $REMOTE_CHAINID --priv-key $KEY --amount 1 &
+sleep .1
+go run main.go --geth-origin $GETH_REMOTE --geth-remote $GETH_ORIGIN --origin-chainid $REMOTE_CHAINID --remote-chainid $ORIGIN_CHAINID --priv-key $KEY --amount 2 --router-origin 0xE6E340D132b5f46d1e472DebcD681B2aBc16e57E --router-remote 0x67d269191c92Caf3cD7723F116c85e6E9bf55933 & 
+
+wait
--- hyperlane-cross-rollup/scripts/send_many_from_same.sh
+++ hyperlane-cross-rollup/scripts/send_many_from_same.sh
@@ -6,7 +6,8 @@ ORIGIN_CHAINID=45205
 REMOTE_CHAINID=45206
 KEY=47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a
 
-go run main.go --geth-origin $GETH_ORIGIN --geth-remote $GETH_REMOTE --origin-chainid $ORIGIN_CHAINID --remote-chainid $REMOTE_CHAINID --priv-key $KEY --nonce-inc 0 &
-go run main.go --geth-origin $GETH_ORIGIN --geth-remote $GETH_REMOTE --origin-chainid $ORIGIN_CHAINID --remote-chainid $REMOTE_CHAINID --priv-key $KEY --nonce-inc 1 &
+go run main.go --geth-origin $GETH_ORIGIN --geth-remote $GETH_REMOTE --origin-chainid $ORIGIN_CHAINID --remote-chainid $REMOTE_CHAINID --priv-key $KEY --nonce-inc 0 --amount 1 &
+sleep .1
+go run main.go --geth-origin $GETH_ORIGIN --geth-remote $GETH_REMOTE --origin-chainid $ORIGIN_CHAINID --remote-chainid $REMOTE_CHAINID --priv-key $KEY --nonce-inc 1 --amount 2 & 
 
 wait
